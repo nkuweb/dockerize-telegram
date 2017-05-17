aa15e5e78f3779d12018630a1a6ac113c5f3fc32
admanage <asddeneme3@gmail.com>
2017-05-18 01:45:10 +0300

    DB conenction added

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM python:latest
 COPY /web /web
 WORKDIR /web
-RUN apt-get update -y
-RUN apt-get install -y python3-setuptools
-RUN pip3 install -r req-web.txt
 
+RUN pip3 install -r req-web.txt
--- web/web.py
+++ web/web.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 from flask import Flask,make_response,render_template,request
+from pymongo MongoClient
 app = Flask(__name__)
+db=MongoClient('db')
 @app.route("/")
 def index():
-    return "<h1 style=margin-left:20em;margin-top:20em;>Hayırsız Leylaa...</h1>"
+    datas=["hello","world","I","love","everyone"]
+    return render_template("index.html",datas=datas)
+
+
 app.run(debug=True,host='0.0.0.0')
