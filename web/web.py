0428b7161748f74c49a142b0a7147a8fe4e63e4e
admanage <asddeneme3@gmail.com>
2017-05-18 01:55:38 +0300

    WebSite Created

--- web/web.py
+++ web/web.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 from flask import Flask,make_response,render_template,request
-from pymongo MongoClient
+from pymongo import MongoClient
 app = Flask(__name__)
-db=MongoClient('db')
+
 @app.route("/")
 def index():
-    datas=["hello","world","I","love","everyone"]
-    return render_template("index.html",datas=datas)
+    db=MongoClient('db')
+    client = db.telegram
+    fetcher  = client.telegramcollection
+    return render_template("index.html",datas=fetcher.find())
 
 
 app.run(debug=True,host='0.0.0.0')
