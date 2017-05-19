961814335dddd3d371e69743e9f33861acdcb129
admanage <asddeneme3@gmail.com>
2017-05-19 16:29:20 +0300

    NGINX ADDE

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM python:latest
-COPY /web /web
-WORKDIR /web
-
-RUN pip3 install -r req-web.txt
--- vm_scripts/proxy_configure.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-sudo -s
-cat flask_config > \
-		/etc/nginx/sites-enabled \
-		/etc/nginx/sites-enabled
-service nginx restart
